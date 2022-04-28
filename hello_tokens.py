096bec8f4c688d07432e83c86d1d63ee5e48203a
Mariane Goncalves Kulik <mari.ti@gmail.com>
2022-04-28 02:43:34 +0200

    Tried a new approach to limit the line breaks and later try to identify titles. Got better but the footnotes and figures in the middle of the text are still a problem.

--- hello_tokens.py
+++ hello_tokens.py
@@ -12,10 +12,37 @@ import os, re
 #/home/magoncal/Documents/data/papers/lab_papers/lab_text/
 path_to_files = input('Add the path where the papers are available as ".TXT": ')
 files = sorted([f.path for f in os.scandir(path_to_files) if (re.search(r'\W*.txt', f.path))])
+
+for comp in files:
+    with open(comp) as f:
+        lines = f.readlines()
+        new_line = ""
+        new_lines = []
+        i=0
+        for line in lines:
+            i+=1
+            if (line!="\n"):
+                line = line.rstrip("\n")
+                sep_line = line.split(" ")
+                if len(sep_line)==1:
+                    new_lines.append(line)
+                    title=True
+                else:
+                    new_line = new_line+" "+line
+                    title=False
+            else:
+                if not title:
+                    new_lines.append(new_line)
+                new_line = ""
+        
+
+
+
+
 for comp in files:
     data = open(comp, 'r').read()
     main_subs = ["abstract", "introduction", "materiais*methods", "results*discussion"]
-    r'(?<={0})(.*?)(?={1})'
+    #r'(?<={0})(.*?)(?={1})'
     for row in data:
         row_main_subs = [(m.start(0), m.end(0)) for m in re.finditer(r'\b({0})\b'.format("abstract"), row)]
         
